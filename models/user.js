40226a09bd486cb58762e5c3c16612ab324f8d93
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-24 22:36:14 -0500

    extended user model

--- models/user.js
+++ models/user.js
@@ -1,12 +1,44 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+        },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        },
+    // thoughts: [
+    //     {
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'Thoughts',
+    //     }
+    // ],
+    // friends: [
+    //     {
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'User',
+    //     }
+    // ],
     lastAccessed: { type: Date, default: Date.now },
-});
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
-const User = mongoose.model('User',userSchema);
+// userSchema.virtual('friendCount')
+
+const User = mongoose.model('User', userSchema);
 
 // const handleError = (err) => console.error(err);
 
