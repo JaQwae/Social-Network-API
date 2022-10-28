0875d6c55347b98d3ffea35609c1961bf98c8218
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-27 21:44:56 -0500

    getting a single thought is working

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 
 const {
-    getThoughts
+    getThoughts,
+    createThought,
+    getSingleThought
 } = require('../../controller/thoughtController');
 
 // /api/thoughts
-router.route('/').get(getThoughts)// .post(createVideo);
+router.route('/').get(getThoughts).post(createThought);
 
+//  /api/thoughts/thoughts:id
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
 module.exports = router;
