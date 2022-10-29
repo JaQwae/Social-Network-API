2f4e3616ba42f8c1e359dfa2fa3df55121dea5c1
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-28 21:35:55 -0500

    v is no longer displaying on get all thoughts

--- controller/thoughtController.js
+++ controller/thoughtController.js
@@ -4,6 +4,7 @@ module.exports = {
     // get all thoughts
     getThoughts(req, res) {
         Thought.find()
+        .select('-__v')
             .then((Thoughts) => res.json(Thoughts))
             .catch((err) => res.status(500).json(err));
     },
